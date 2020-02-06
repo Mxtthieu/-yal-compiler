7e0b1ba3531237b709fa562f59c2fc092d46cbf5
Matthieu Perrigot <matthieu.perrigot5@etu.univ-lorraine.fr>
2020-02-06 15:40:50 +0100

    yal2 : Operateur debut

--- yal/yal/arbre/expressions/binaire/Binaire.java
+++ yal/yal/arbre/expressions/binaire/Binaire.java
@@ -4,20 +4,35 @@ import yal.arbre.expressions.Expression;
 
 public abstract class Binaire extends Expression {
 
-    private Expression exp1;
-    private Expression exp2;
+    private Expression gauche;
+    private Expression droite;
 
-    protected Binaire(int n) {
-        super(n);
+    protected Binaire(Expression exp1, Expression exp2) {
+        super(exp1.getNoLigne());
+        this.gauche = exp1;
+        this.droite = exp2;
     }
 
     @Override
     public void verifier() {
-        
+        gauche.verifier();
+        droite.verifier();
     }
 
     @Override
     public String toMIPS() {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append("# Partie de gauche \n");
+        sb.append(gauche.toMIPS());
+        sb.append("\n");
+        sb.append("# Empilement de la partie de gauche");
+        sb.append("sw $v0, 0($sp)\n");
+        sb.append("add $sp, $sp, -4\n\n");
+        sb.append("# Partie de droite");
+        sb.append(droite.toMIPS());
+        sb.append("# Depilement de la partie de gauche");
+        sb.append("add $sp, $sp, 4\n");
+        sb.append("lw $t8, 0($sp)\n\n");
+        return sb.toString();
     }
 }
---
+++ yal/yal/arbre/expressions/binaire/operateur/arithmetique/ArithmetiqueBinaire.java
@@ -0,0 +1,14 @@
+package yal.arbre.expressions.binaire.operateur.arithmetique;
+
+import yal.arbre.expressions.Expression;
+import yal.arbre.expressions.binaire.Binaire;
+
+public class ArithmetiqueBinaire extends Binaire {
+
+
+    protected ArithmetiqueBinaire(Expression exp1, Expression exp2) {
+        super(exp1, exp2);
+    }
+
+
+}
